fa3c21b245d53b8fb7fed2413ba3524c5c69ba54
Jack Schaible <jack.schaible@hotmail.com>
2013-12-21 19:19:00 -0700

    Global Marketplace Update

--- CouponsForGiving/Default/GlobalMarketplace.aspx.cs
+++ CouponsForGiving/Default/GlobalMarketplace.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using CouponsForGiving;
+using CouponsForGiving.Data;
+using CouponsForGiving.Data.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,18 @@ using System.Web.UI.WebControls;
 
 public partial class Default_GlobalMarketplace : System.Web.UI.Page
 {
+    public List<DealInstance> DIs;
     protected void Page_Load(object sender, EventArgs e)
     {
         Controls_MenuBar control = (Controls_MenuBar)Master.FindControl("MenuBarControl");
         control.MenuBar = MenuBarType.Supporter;
+
+        if (!IsPostBack)
+            BindData();
+    }
+
+    private void BindData()
+    {
+        DIs = DealInstances.ListForGlobalMarketplace();
     }
 }
